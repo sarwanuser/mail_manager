0d327687de4e2b9bb8b948c12b984698e7482acc
Sarwan Verma <sarwandeveloper@gmail.com>
2025-09-25 23:26:24 +0530

    update

--- app/Http/Controllers/ClassSessionController.php
+++ app/Http/Controllers/ClassSessionController.php
@@ -28,7 +28,7 @@ class ClassSessionController extends Controller
      * @return Response
      */
     public function generateClassSessions(Request $request){
-        
+        return $request->all();
         $validator = Validator::make($request->all(), [ 
             'sub_category_id' => 'required',
             'package_id' => 'required',
