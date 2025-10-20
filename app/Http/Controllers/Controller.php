f81f0b997542572e18a959bba0b89735678a033b
Sarwan Verma <sarwandeveloper@gmail.com>
2025-10-20 15:23:57 +0530

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1114,8 +1114,9 @@ class Controller extends BaseController
                         $message->to($cust['email']);
                         $message->subject('Class Invite - '.$cust['class']);
                     });
-                    dd('update invites set sent=1 where id='.$data->id);
-                    DB::connection('clykk_lifestyle')->raw('update invites set sent=1 where id='.$data->id);
+                    
+
+                    DB::connection('clykk_lifestyle')->statement('UPDATE invites SET sent = ? WHERE id = ?', ['1', $data->id]);
                 }
 
                 dd($datas);
